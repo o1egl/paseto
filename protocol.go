914e5130c0b48134d04bb30cea712455f9f5704e
Aidan Woods <aidantwoods@gmail.com>
2021-10-04 23:22:47 +0100

    Update interfaces
    Rather than accepting bytes we accept a generalised key type associated
    with its function.
    Keys are a natural place for encrypt implementations associated with
    that particular key to live. This also allows us to gate the
    unitTestNonce behind a non-exported method.

--- protocol.go
+++ protocol.go
@@ -1,8 +1,6 @@
 package paseto
 
 import (
-	"crypto"
-
 	errors "golang.org/x/xerrors"
 )
 
@@ -11,12 +9,14 @@ var (
 	ErrUnsupportedTokenVersion = errors.New("unsupported parser version")
 	// ErrUnsupportedTokenType unsupported token type
 	ErrUnsupportedTokenType = errors.New("unsupported token type")
-	// ErrIncorrectPrivateKeyType incorrect private key type
-	ErrIncorrectPrivateKeyType = errors.New("incorrect private key type")
-	// ErrIncorrectPublicKeyType incorrect public key type
-	ErrIncorrectPublicKeyType = errors.New("incorrect public key type")
+	// ErrWrongKeyType incorrect key given for PASETO operation
+	ErrWrongKeyType = errors.New("the given key is not intended for this version of PASETO")
+	// ErrWrongKeyLength incorrect key given for PASETO operation
+	ErrWrongKeyLength = errors.New("the given key is the wrong length for this type of key")
 	// ErrPublicKeyNotFound public key for this version not found
 	ErrPublicKeyNotFound = errors.New("public key for this version not found")
+	// ErrSecretKeyNotFound public key for this version not found
+	ErrSecretKeyNotFound = errors.New("secret key for this version not found")
 	// ErrIncorrectTokenFormat incorrect token format
 	ErrIncorrectTokenFormat = errors.New("incorrect token format")
 	// ErrIncorrectTokenHeader incorrect token header
@@ -31,22 +31,47 @@ var (
 	ErrTokenValidationError = errors.New("token validation error")
 )
 
+type SymmetricKey interface {
+
+	// Encrypt encrypts a token with a symmetric key. The key should be a byte
+	// slice of 32 bytes, regardless of whether PASETO v1 or v2 is being used.
+	encrypt(payload interface{}, footer interface{}, unitTestNonce []byte) (string, error)
+
+	// Decrypt decrypts a token which was encrypted with a symmetric key.
+	decrypt(token string, payload interface{}, footer interface{}) error
+}
+
+type AsymmetricSecretKey interface {
+
+	// Sign signs a token with the given private key. For PASETO v1, the key should
+	// be an rsa.PrivateKey. For v2, the key should be an ed25519.PrivateKey.
+	sign(payload interface{}, footer interface{}) (string, error)
+}
+
+type AsymmetricPublicKey interface {
+
+	// Verify verifies a token against the given public key. For PASETO v1, the key
+	// key should be an rsa.PublicKey. For v2, the key should be an
+	// ed25519.PublicKey.
+	verify(token string, value interface{}, footer interface{}) error
+}
+
 // Protocol defines the PASETO token protocol interface.
 type Protocol interface {
 
 	// Encrypt encrypts a token with a symmetric key. The key should be a byte
 	// slice of 32 bytes, regardless of whether PASETO v1 or v2 is being used.
-	Encrypt(key []byte, payload interface{}, footer interface{}) (string, error)
+	Encrypt(key SymmetricKey, payload interface{}, footer interface{}) (string, error)
 
 	// Decrypt decrypts a token which was encrypted with a symmetric key.
-	Decrypt(token string, key []byte, payload interface{}, footer interface{}) error
+	Decrypt(token string, key SymmetricKey, payload interface{}, footer interface{}) error
 
 	// Sign signs a token with the given private key. For PASETO v1, the key should
 	// be an rsa.PrivateKey. For v2, the key should be an ed25519.PrivateKey.
-	Sign(privateKey crypto.PrivateKey, payload interface{}, footer interface{}) (string, error)
+	Sign(privateKey AsymmetricSecretKey, payload interface{}, footer interface{}) (string, error)
 
 	// Verify verifies a token against the given public key. For PASETO v1, the key
 	// key should be an rsa.PublicKey. For v2, the key should be an
 	// ed25519.PublicKey.
-	Verify(token string, publicKey crypto.PublicKey, value interface{}, footer interface{}) error
+	Verify(token string, publicKey AsymmetricPublicKey, value interface{}, footer interface{}) error
 }
